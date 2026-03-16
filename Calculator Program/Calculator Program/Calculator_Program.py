b537de2c554ab0326e6081f439beff983493c8d4
Anirudh Muthurajan Rajasreee <anirudh.mr@hotmail.com>
2026-03-16 18:17:14 +0530

    Add files via upload

---
+++ Calculator Program/Calculator Program/Calculator_Program.py
@@ -0,0 +1,18 @@
+print("Welcome Anirudh's Calculator Program!")
+print("kindly enter a number:")
+num1 = float(input())
+print("Enter another number:")
+num2 = float(input())
+sum = num1 + num2
+difference = num1 - num2
+product = num1 * num2
+quotient = num1 / num2
+Exponentiation = num1 ** num2
+print("The sum of", num1, "and", num2, "is:", sum)
+print("The difference of", num1, "and", num2, "is:", difference)
+print("The product of", num1, "and", num2, "is:", product)
+print("The quotient of", num1, "and", num2, "is:", quotient)
+print("The Exponentiation of", num1, "and", num2, "is:", Exponentiation)
+
+
+
