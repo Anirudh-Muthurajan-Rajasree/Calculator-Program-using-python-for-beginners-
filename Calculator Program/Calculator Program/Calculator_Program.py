bbe64bf364dd0ec95d32df93e357b25ae9f769f5
Anirudh Muthurajan Rajasreee <anirudh.mr@hotmail.com>
2026-03-28 21:24:29 +0530

    Refactor calculator program with comments

--- Calculator Program/Calculator Program/Calculator_Program.py
+++ Calculator Program/Calculator Program/Calculator_Program.py
@@ -1,13 +1,17 @@
+#Created by Anirudh M.R
 print("Welcome Anirudh's Calculator Program!")
+#Asking the user to type a number
 print("kindly enter a number:")
-num1 = float(input())
+num1 = float(input())#coverting the Datatype from string to float
 print("Enter another number:")
 num2 = float(input())
+#Creating answers for different types of operations
 sum = num1 + num2
 difference = num1 - num2
 product = num1 * num2
 quotient = num1 / num2
 Exponentiation = num1 ** num2
+#Printing th values on the console
 print("The sum of", num1, "and", num2, "is:", sum)
 print("The difference of", num1, "and", num2, "is:", difference)
 print("The product of", num1, "and", num2, "is:", product)
